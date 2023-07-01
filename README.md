8dcd838b843c13e3007477a9cabac376404fdd71
Ken Chigozie <kenronkwo@gmail.com>
2023-07-01 15:13:06 +0100

    Exercise and Fitness

---
+++ README.md
@@ -0,0 +1,3 @@
+## REACT FITNESS AND EXERCISE APPLICATION
+
+![React Fitness and Exercise Application](https://i.ibb.co/Yt9spGc/image.png)
---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': '95bf466164mshc3230fc46ed6f12p1313ccjsnf1bd29da80df',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
